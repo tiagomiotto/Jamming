320b946bdce9ecd4210337f32839de7f99eea0e7
tiagomiotto <tiago.miotto@hotmail.com>
2023-02-10 16:33:21 +0000

    Create App component

---
+++ src/Components/App/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+import {SearchBar} from './SearchBar'
+import {SearchResults} from './SearchResults'
+import {Playlist} from './Playlist'
+
+function App() {
+  return (
+    <div>
+    <h1>Ja<span className="highlight">mmm</span>ing</h1>
+    <div className="App">
+      <SearchBar />
+      <div className="App-playlist">
+        <SearchResults />
+        <Playlist />
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default App;
