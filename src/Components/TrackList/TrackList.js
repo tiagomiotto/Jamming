41f2b52e34305c579db797b6cf7321a1545658af
tiagomiotto <tiago.miotto@hotmail.com>
2023-02-10 16:47:05 +0000

    Add TrackList component

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,12 @@
+import "./TrackList.css";
+import React from "react";
+
+export class TrackList extends React.Component {
+  render() {
+    return (
+      <div class="TrackList">
+       
+      </div>
+    );
+  }
+}
